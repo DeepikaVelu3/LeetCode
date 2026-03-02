95810b03dc620e5d40a78b681594964163a226cc
DeepikaVelu3 <deepikavelu003@gmail.com>
2026-03-02 09:24:01 +0530

    Time: 0 ms (100%), Space: 42.9 MB (58.08%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        String s3= s+s;
+        for (int i=0; i<=s3.length()-goal.length();i++) {
+            int j = 0;
+            while (j<goal.length() &&
+                   s3.charAt(i + j) == goal.charAt(j)) {
+                j++;
+            }
+            if (j==goal.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
