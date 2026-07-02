efcac2a8731ffd5dfd3048b6eff6cf427148dfee
DeepikaVelu3 <deepikavelu003@gmail.com>
2026-07-02 09:42:01 +0530

    Time: 7 ms (74.3%), Space: 46 MB (90.5%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.java
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.java
@@ -1,17 +1,20 @@
 class Solution {
     public int countWords(String[] words1, String[] words2) {
-      HashMap<String,Integer> mp1 = new HashMap<>(); 
-      HashMap<String,Integer> mp2 = new HashMap<>();
       int count = 0;
-      for(String str1 : words1){
-            mp1.put(str1,mp1.getOrDefault(str1,0)+1);
+      HashMap<String , Integer> mp  = new HashMap<>();
+      HashMap<String , Integer> mp2 = new HashMap<>();
+      for(int i =0 ; i< words1.length; i++){
+        String s = words1[i];
+        mp.put(s , mp.getOrDefault(s,0)+1);
+
       } 
-      for(String str2 : words2){
-        mp2.put(str2,mp2.getOrDefault(str2,0)+1);
-      }
-      for(int i =0; i < words1.length;i++){
-        String str = words1[i];
-        if(mp1.getOrDefault(str,0) == 1 && mp2.getOrDefault(str,0)==1 ){
+      for(int i =0 ; i< words2.length; i++){
+        String s = words2[i];
+        mp2.put(s , mp2.getOrDefault(s,0)+1);
+
+      }   
+      for(int i = 0 ; i < words1.length; i++){
+        if(mp.get(words1[i]) == 1 && mp2.getOrDefault(words1[i] , 0) == 1){
             count++;
         }
       }
